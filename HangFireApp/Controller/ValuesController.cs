d66ad3c5439c70a376f066345e0b803b2d4f4d23
ismailaydemir <67390681+ismailaydemirx@users.noreply.github.com>
2025-05-19 21:11:51 +0300

    create hangfire and add manuel test function

---
+++ HangFireApp/Controller/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangFireApp.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            BackgroundJob.Enqueue(() => BackgroundServives.HangFireWorks());
+            return Ok("Hangfire worked.");
+        }
+    }
+
+    public class BackgroundServives
+    {
+        public static void HangFireWorks()
+        {
+            Console.WriteLine($"Hangfire is working: {DateTime.UtcNow}");
+        }
+    }
+}
---
+++ HangFireApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using Hangfire;
+using HangFireApp.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = builder.Configuration.GetConnectionString("SqlServer");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+// Add services to the container.
+builder.Services.AddHangfire(config =>
+{
+    config.UseSqlServerStorage(connectionString);
+});
+builder.Services.AddHangfireServer();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Görselli dashboard çıkartmak için kullanıyoruz.
+app.UseHangfireDashboard();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
